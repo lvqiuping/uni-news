514f5346c2f2c731dc80f49433e499cecb7bb6ce
arron1118 <arron1118@icloud.com>
2022-08-26 18:20:36 +0800

    update

--- components/vol-list/message-list.vue
+++ components/vol-list/message-list.vue
@@ -1,34 +1,5 @@
 <template>
 	<block>
-		<view style="margin-bottom: 10px;">
-			<u-row justify="space-between">
-				<u-col span="12">
-					<u-swiper :list="swiperList" :radius="0" indicator indicatorMode="dot" circular
-						indicatorActiveColor="#FC5C5B" />
-				</u-col>
-			</u-row>
-		</view>
-		<u-row justify="space-between" style="margin-bottom: 10px;">
-			<u-col span="12">
-				<!-- <view class="news-info">
-							<view class="news-info-inner">
-								<u-row>
-									<u-col span="4">
-										<view class="n-left">
-											<u--image width="80rpx" height="80rpx" :src="localImage1"></u--image>
-											<view class="n-tips">快讯</view>
-										</view>
-									</u-col>
-									<u-col span="8">
-										<view class="n-right over_two_lines"></view>
-									</u-col>
-								</u-row>
-						
-							</view>
-						</view> -->
-			</u-col>
-		</u-row>
-
 		<block v-if="list.length > 0">
 			<view v-for="(item, index) in list" style="padding-bottom: 10px;" :key="index">
 				<u-row justify="space-between" gutter="10" @click="toDetail(item)">
@@ -41,7 +12,7 @@
 						<u--image width="80px" height="80px" radius="15px" :src="item.cover_img || ''"></u--image>
 					</u-col>
 				</u-row>
-				<u-line></u-line>
+				<u-line margin="10px 0"></u-line>
 			</view>
 		</block>
 		<block v-else>
@@ -52,7 +23,6 @@
 			</u-row>
 		</block>
 	</block>
-
 </template>
 
 <script>
@@ -63,20 +33,6 @@
 				default: () => []
 			}
 		},
-		data() {
-			return {
-				localImage1: '/static/imgs/2x.png',
-				swiperList: [
-					'/static/swiper1.png',
-					'/static/swiper2.png',
-					'/static/swiper3.png'
-				]
-			}
-		},
-		created() {},
-		// #ifdef MP-WEIXIN
-		watch: {},
-		// #endif
 		methods: {
 			toDetail(item) {
 				const userInfo = uni.getStorageSync('userInfo')
